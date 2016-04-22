2b9305d3cf3e08b0ee052a752d54038521279e68
Ryan Yogan <ryanyogan@gmail.com>
2016-04-22 15:33:48 -0500

    Complete, silly little cat scroller

--- src/cljs/swagger_service/core.cljs
+++ src/cljs/swagger_service/core.cljs
@@ -1,6 +1,7 @@
 (ns swagger-service.core
   (:require [reagent.core :as reagent :refer [atom]]
-            [ajax.core :refer [GET]])
+            [ajax.core :refer [GET]]
+            [goog.string :as gs])
   (:require-macros [secretary.core :refer [defroute]]))
 
 ;;
@@ -24,7 +25,34 @@
      [:div.row
       (for [link row]
         ^{:key link}
-        [:div.col-sm-4 [:img {:width 400 :src link}]])])])
+        [:div.col-sm-4 [:img {:width 200 :height 200 :src link}]])])])
+
+(defn forward [i pages]
+  (if (< i (dec pages)) (inc i) i))
+
+(defn back [i]
+  (if (pos? i) (dec i) i))
+
+(defn nav-link [page i]
+  [:li.page-item>a.page-link.btn.btn-primary
+   {:on-click #(reset! page i)
+    :class (when (= i @page) "active")}
+   [:span i]])
+
+(defn pager [pages page]
+  (when (> pages 1)
+    (into
+     [:div.text-xs-center>ul.pagination.pagination-lg]
+     (concat
+      [[:li.page-item>a.page-link.btn
+        {:on-click #(swap! page back pages)
+         :class (when (= @page 0) "disabled")}
+        [:span (gs/unescapeEntities "&laquo;")]]]
+      (map (partial nav-link page) (range pages))
+      [[:li.page-item>a.page-link.btn
+        {:on-click #(swap! page forward pages)
+         :class (when (= @page (dec pages)) "disabled")}
+        [:span (gs/unescapeEntities "&raquo;")]]]))))
 
 ;;
 ;; Page Components
@@ -34,11 +62,13 @@
   (let
    [links (atom nil)
     page (atom 0)]
-    (fetch-links! links 20)
+    (fetch-links! links 50)
     (fn []
-      [:div
-       (when @links
-         [images (@links @page)])])))
+      (if (not-empty @links)
+        [:div.container>div.row>div.col-md-12
+         [pager (count @links) page]
+         [images (@links @page)]]
+        [:div "Standby for cats!"]))))
 
 ;;
 ;; Component Mounting
