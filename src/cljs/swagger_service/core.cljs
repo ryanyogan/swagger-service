af8237a943ff7071188d252f64936cdfed7d037e
Ryan Yogan <ryanyogan@gmail.com>
2016-04-22 15:18:50 -0500

    added in a partition-all 6 grid

--- src/cljs/swagger_service/core.cljs
+++ src/cljs/swagger_service/core.cljs
@@ -11,7 +11,20 @@
   "Fetches links from api, returns an array the length of link-count"
   (GET "/api/cat-links"
     {:params  {:link-count link-count}
-     :handler #(reset! links %)}))
+     :handler #(reset! links (vec (partition-all 6 %)))}))
+
+;;
+;; Helper Components
+;;
+
+(defn images [links]
+  [:div.text-xs-center
+   (for [row (partition-all 3 links)]
+     ^{:key row}
+     [:div.row
+      (for [link row]
+        ^{:key link}
+        [:div.col-sm-4 [:img {:width 400 :src link}]])])])
 
 ;;
 ;; Page Components
@@ -19,12 +32,13 @@
 
 (defn home-page []
   (let
-   [links (atom nil)]
+   [links (atom nil)
+    page (atom 0)]
     (fetch-links! links 20)
     (fn []
       [:div
-       (for [link @links]
-         [:img {:src link}])])))
+       (when @links
+         [images (@links @page)])])))
 
 ;;
 ;; Component Mounting
