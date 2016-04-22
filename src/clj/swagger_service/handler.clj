df962e66fcbf8eadc3c9edec9389ef7da69febd9
Ryan Yogan <ryanyogan@gmail.com>
2016-04-22 13:18:52 -0500

    Initial Commit

---
+++ src/clj/swagger_service/handler.clj
@@ -0,0 +1,20 @@
+(ns swagger-service.handler
+  (:require [compojure.core :refer [routes wrap-routes]]
+            [swagger-service.layout :refer [error-page]]
+            [swagger-service.routes.home :refer [home-routes]]
+            [swagger-service.routes.services :refer [service-routes]]
+            [compojure.route :as route]
+            [swagger-service.middleware :as middleware]))
+
+(def app-routes
+  (routes
+    #'service-routes
+    (-> #'home-routes
+        (wrap-routes middleware/wrap-csrf)
+        (wrap-routes middleware/wrap-formats))
+    (route/not-found
+      (:body
+        (error-page {:status 404
+                     :title "page not found"})))))
+
+(defn app [] (middleware/wrap-base #'app-routes))
